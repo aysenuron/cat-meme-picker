99b1dea41aba25c1d2dee8cfb71fbd16557932e8
aysenuron <onaranays@gmail.com>
2025-02-07 14:21:54 +0300

    Filter radio selected cats and gif only.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { catsData  } from "./data.js";
 
 const emotionRadios = document.getElementById('emotion-radios');
 const getImageBtn = document.getElementById("get-image-btn");
+const gifsOnlyOption = document.getElementById("gifs-only-option");
 
 function getEmotionsArray(cats){
     const emotionsArray = []
@@ -42,8 +43,19 @@ function highlightCheckedOption(e) {
 
 function getMatchingCatsArray() {
     if(document.querySelector("input[type='radio']:checked")) {
+        const isGif = gifsOnlyOption.checked;
+
         const selectedEmotion = document.querySelector("input[type='radio']:checked").value;
         console.log(selectedEmotion);
+        
+        const matchingCatsArray = catsData.filter((cat) => {
+            if(isGif) {
+                return cat.emotionTags.includes(selectedEmotion) && cat.isGif
+            } else {
+                return cat.emotionTags.includes(selectedEmotion)
+            }
+        });
+        console.log(matchingCatsArray)
     }
 }
 
